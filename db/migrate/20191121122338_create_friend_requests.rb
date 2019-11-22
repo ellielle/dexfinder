c1a5dad1ed22edfa46d024d3e40f5cfb94d2866b
ellielle <40385743+ellielle@users.noreply.github.com>
2019-11-22 01:52:27 -0500

    add friendrequest model

---
+++ db/migrate/20191121122338_create_friend_requests.rb
@@ -0,0 +1,9 @@
+class CreateFriendRequests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friend_requests do |t|
+      t.user :references
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/friend_requests.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :friend_request do
+    references { "" }
+  end
+end
