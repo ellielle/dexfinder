d7c383433361b27168ab2cd71cfa0db7c516f42e
ellielle <40385743+ellielle@users.noreply.github.com>
2019-11-26 00:49:02 -0500

    update routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  # Devise routes
   devise_for :users, controllers: {
       registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'sign_in', to: "devise/sessions#new"
+    get 'sign_up', to: "users/registrations#new"
+    get 'sign_out', to: "devise/sessions#destroy"
+  end
 end
