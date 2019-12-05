a22927f2772733c9dd9b25a686952e6989b089a2
ellielle <40385743+ellielle@users.noreply.github.com>
2019-12-05 05:06:54 -0500

    add redirect if user isn't logged in

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,9 @@ module PagesHelper
   def user_has_request?
     current_user.friend_requests.any?
   end
+
+  def user_signed_in_check
+    flash[:danger] = "You must be signed in to visit this page."
+    redirect_to root_url unless user_signed_in?
+  end
 end
