3d1119de181fdfeef9fa0d47d26f86bba75494f1
ellielle <40385743+ellielle@users.noreply.github.com>
2019-12-05 02:17:53 -0500

    add user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
-    
+    id { 1 }
+    username { "testy" }
+    email { "example@email.com" }
+    password { "testing12" }
+
+    trait :user2 do
+      id { 2 }
+      username { "user2" }
+      email { "fake@user.com" }
+      password { "totallynotuser1" }
+    end
   end
 end
