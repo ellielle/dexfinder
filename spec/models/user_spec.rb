7a9fe9043e4ae155966216921c11c511b678b389
ellielle <40385743+ellielle@users.noreply.github.com>
2019-12-05 03:00:53 -0500

    update user factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending
+  describe "when test users are created" do
+    it "should be valid user" do
+      user = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user, :user2)
+      expect(user).to be_valid
+      expect(user2).to be_valid
+    end
+  end
 end
