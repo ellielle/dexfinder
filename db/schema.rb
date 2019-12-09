2c7494cdeb2d03598858b6388c401ff39d926d46
ellielle <40385743+ellielle@users.noreply.github.com>
2019-12-09 04:18:02 -0500

    add friend status to friend requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_071236) do
+ActiveRecord::Schema.define(version: 2019_12_09_090119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_071236) do
     t.integer "to_user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "none"
     t.index ["from_user_id"], name: "index_friend_requests_on_from_user_id"
     t.index ["to_user_id"], name: "index_friend_requests_on_to_user_id"
   end
