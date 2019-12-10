167fee4432b5bd4d5b0b0628add72f1017010ddc
ellielle <40385743+ellielle@users.noreply.github.com>
2019-12-10 06:40:37 -0500

    fix tests with db cleaner

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :user do
+    id { 1 }
     username { Faker::Internet.user_name }
     email { Faker::Internet.email }
     password { "testing12" }
 
     trait :user2 do
+      id { 2 }
       username { Faker::Internet.user_name }
       email { Faker::Internet.email }
       password { "totallynotuser1" }
--- spec/system/requesting_friends_spec.rb
+++ spec/system/requesting_friends_spec.rb
@@ -1,22 +1,47 @@
 require 'rails_helper'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :transaction
+RSpec.configure do |config|
+  config.after :each do
+    Warden.test_reset!
+  end
+end
 
 RSpec.describe "RequestingFriends", type: :system do
   before do
     driven_by(:rack_test)
-    @user = FactoryBot.create(:user)
-    sign_in @user
+    DatabaseCleaner.start
+    user = FactoryBot.create(:user)
+    login_as(user, scope: :user)
   end
 
-  describe "navigating to profile page" do
+  after do
+    DatabaseCleaner.clean
+  end
+
+  describe "navigating to profile page and accepting invitation" do
     it "should allow user into their profile page" do
-      get profile_url
-      expect(response).to have_http_status(200)
+      FactoryBot.create(:user, :user2)
+      FactoryBot.create(:friend_request)
+      visit profile_url
+      expect(page.status_code).to eq(200)
+      expect(page).to have_current_path(profile_path)
+      expect(page).to have_css(".pending-incoming-requests")
+      #click accept button for each request
+      #ensure none are left
+      #reset test / requests?
+      #navigate back
+      #click decline button for each request
+      #ensure none are left
     end
 
-    it "should redirect user from profile page if not signed in" do
-      sign_out @user
-      get profile_url
-      expect(response).to have_http_status(302)
+    it "should redirect user to root page if not signed in" do
+      logout
+      visit profile_url
+      expect(page.status_code).to eq(200)
+      expect(page).to have_current_path(root_path)
+      expect(page).to_not have_css(".pending-incoming-requests")
     end
   end
 end
