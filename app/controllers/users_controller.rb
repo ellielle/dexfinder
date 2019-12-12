071e185ccf44fdcdddef5c3a8b7e45cd1052d0c4
ellielle <40385743+ellielle@users.noreply.github.com>
2019-12-12 01:08:05 -0500

    update some stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @requests = current_user.friend_requests if user_has_request?
+    @friend_requests = current_user.friend_requests if user_has_request?
+    respond_to do |format|
+      format.html
+      format.js
+    end
+    end
+=begin
+  def update
+    respond_to do |format|
+      if params[:commit] == "Accept"
+        format.html { redirect_to root_url }
+        format.js
+      elsif params[:commit] == "Decline"
+        format.js
+      end
+
+    end
   end
+=end
 end
---
+++ app/helpers/friends_helper.rb
@@ -0,0 +1,13 @@
+module FriendsHelper
+  def accept_invite
+
+  end
+
+  def reject_invite
+
+  end
+
+  def block_user
+
+  end
+end
