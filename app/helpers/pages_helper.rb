fca5b1e7a4ee7f54f9d2846db1493dee2b1aeb10
ellielle <40385743+ellielle@users.noreply.github.com>
2019-12-16 11:03:07 -0500

    refactor #user_has_request?

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def user_has_request?
-    current_user.friend_requests.any?
+    current_user.friend_requests.where(to_user_id: current_user.id, status: "none").any?
   end
 
   def user_signed_in_check
