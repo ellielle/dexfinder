f40e6a6bbcb9c7cc27b0640b4199de475e181244
ellielle <40385743+ellielle@users.noreply.github.com>
2020-01-09 05:25:11 -0500

    refactor how posts are shown

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,23 +3,28 @@ class LikesController < ApplicationController
   before_action :find_post
 
   def create
-    unless already_liked?(params[:post_id])
-      @post.likes.create
+    if already_liked?(@post.id)
+      destroy
+    else
+      if @post.likes.create(user_id: current_user.id, post_id: @post.id)
+        flash.now[:success] = "work"
+      else
+        flash.now[:danger] = "nope"
+      end
     end
+  end
+
+  def destroy
+    @post.likes.find_by(user_id: current_user.id, post_id: @post.id).destroy
+  end
+
+  def set_likes
     respond_to do |format|
       format.js
     end
   end
 
-  def destroy
-
-  end
-
   private
-
-  def like_params
-
-  end
 
   def find_post
     @post = Post.find_by(slug: params[:post_slug])
