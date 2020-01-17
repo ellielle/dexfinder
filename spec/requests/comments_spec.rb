a9929b1fbf8f6bc7518af3d17625580e00596bbd
ellielle <40385743+ellielle@users.noreply.github.com>
2020-01-17 02:52:11 -0500

    add tests

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'support/database_cleaner'
+
+RSpec.describe "Comment tests" do
+  before do
+    @current_user = FactoryBot.create(:user)
+    @other_user = FactoryBot.create(:user, username: Faker::Internet.user_name,
+                                    email: "test@testytest.com",
+                                    password: "boopsie")
+    sign_in(@current_user)
+  end
+
+  def create_posts
+    FactoryBot.create(:post, title: "testing this post", body: "hi i'm testing the body",
+                      user_id: @current_user.id)
+    FactoryBot.create(:post, title: "also hi", body: "also testing body", user_id: @current_user.id)
+  end
+
+  xcontext "when creating a new comment" do
+
+  end
+
+end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,6 +4,9 @@ require 'support/database_cleaner'
 RSpec.describe "User tests" do
   before do
     @current_user = FactoryBot.create(:user)
+    @other_user = FactoryBot.create(:user, username: Faker::Internet.user_name,
+                                    email: "test@testytest.com",
+                                    password: "boopsie")
     sign_in(@current_user)
   end
 
@@ -11,12 +14,6 @@ RSpec.describe "User tests" do
     FactoryBot.create(:post, title: "testing this post", body: "hi i'm testing the body",
                       user_id: @current_user.id)
     FactoryBot.create(:post, title: "also hi", body: "also testing body", user_id: @current_user.id)
-  end
-
-  def create_other_user
-    @other_user = FactoryBot.create(:user, username: Faker::Internet.user_name,
-                                    email: "test@testytest.com",
-                                    password: "boopsie")
   end
 
   context "when creating posts" do
@@ -34,6 +31,8 @@ RSpec.describe "User tests" do
       sign_out(@current_user)
       get new_post_path
       expect(response).to have_http_status(302)
+      follow_redirect!
+      expect(response.body).to include("You must be signed in")
       post posts_path, params: { post: { title: "OI", body: "THIS IS A POST", user_id: @current_user.id }}
       expect(response).to have_http_status(302)
     end
@@ -66,7 +65,6 @@ RSpec.describe "User tests" do
     it "only allows the post author to delete" do
       FactoryBot.create(:post, title: "testing this post", body: "hi i'm testing the body",
                         user_id: @current_user.id)
-      create_other_user
       sign_out(@current_user)
       sign_in(@other_user)
       get post_path(Post.first)
@@ -79,6 +77,36 @@ RSpec.describe "User tests" do
       delete post_path(Post.first)
       expect(response).to have_http_status(302)
       expect(Post.all.count).to eq(0)
+      follow_redirect!
+      expect(response.body).to include("Post deleted.")
+    end
+  end
+
+  context "when creating posts" do
+    it "only allows logged in users to create posts" do
+      expect(Post.all.count).to eq(0)
+      get new_post_path
+      expect(response).to have_http_status(200)
+      post posts_path, params: { post: { title: "test title",
+                                         body: "testing body",
+                                         user_id: @current_user.id }}
+      expect(Post.all.count).to eq(1)
+      expect(response).to have_http_status(302)
+      follow_redirect!
+      expect(response.body).to include("test title")
+      sign_out(@current_user)
+      get new_post_path
+      expect(response).to have_http_status(302)
+      post posts_path, params: { post: { title: "test title",
+                                         body: "testing body",
+                                         user_id: @current_user.id }}
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  xcontext "when editing/updating a post" do
+    it "only allows the proper user to edit their post" do
+
     end
   end
 end
