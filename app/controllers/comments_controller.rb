1ead41c5a34fd7aaaa731fb2bf7988a20618cf00
ellielle <40385743+ellielle@users.noreply.github.com>
2020-01-24 05:28:45 -0500

    change comment count to accurately reflect child comment numbers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :user_signed_in_redirect
   before_action :find_commentable, only: [:create, :destroy]
+  after_action :update_comment_count, only: [:create, :destroy]
 
   def new
     @comment = Comment.new
@@ -17,7 +18,7 @@ class CommentsController < ApplicationController
 
   def destroy
     if @commentable.user == current_user
-      @commentable.destroy unless @commentable.nil?
+      @commentable.update(body: "[deleted]") unless @commentable.nil?
       redirect_back(fallback_location: posts_path)
     else
       flash[:danger] = "You can't do that."
@@ -36,6 +37,15 @@ class CommentsController < ApplicationController
       @commentable = Post.find(params[:comment][:post_id])
     elsif params[:comment][:comment_id]
       @commentable = Comment.find(params[:comment][:comment_id])
+    end
+  end
+
+  def update_comment_count
+    if request.request_method == "POST"
+      @commentable.update(comment_count:  @commentable.comment_count + 1)
+    elsif request.request_method == "DELETE"
+      post = Post.find_by(slug: request.referrer.rpartition(/\//)[2])
+      post.update(comment_count: post.comment_count - 1) unless post.comment_count <= 0
     end
   end
 end
