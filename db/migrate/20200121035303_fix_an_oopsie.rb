73042c215a13a6c7323027bdc256ea89e430b4f1
ellielle <40385743+ellielle@users.noreply.github.com>
2020-02-25 01:53:48 -0500

    once again

--- db/migrate/20200121035303_fix_an_oopsie.rb
+++ db/migrate/20200121035303_fix_an_oopsie.rb
@@ -1,6 +1,5 @@
 class FixAnOopsie < ActiveRecord::Migration[6.0]
   def change
-    rename_column :comments, :comment_type, :commentable_type
     add_reference :comments, :user, foreign_key: true
   end
 end
